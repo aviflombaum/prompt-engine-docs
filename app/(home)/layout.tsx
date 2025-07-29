26567006cee799dfd57bfb21a50fc5c8e4106593
Avi Flombaum <4515+aviflombaum@users.noreply.github.com>
2025-07-29 14:07:19 -0400

    working

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,7 +1,14 @@
-import type { ReactNode } from 'react';
-import { HomeLayout } from 'fumadocs-ui/layouts/home';
-import { baseOptions } from '@/app/layout.config';
+import { DocsLayout } from "fumadocs-ui/layouts/docs";
+import type { ReactNode } from "react";
+import { baseOptions } from "@/app/layout.config";
+import { source } from "@/lib/source";
 
 export default function Layout({ children }: { children: ReactNode }) {
-  return <HomeLayout {...baseOptions}>{children}</HomeLayout>;
+  return (
+    <DocsLayout
+      tree={source.pageTree}
+      {...baseOptions}>
+      {children}
+    </DocsLayout>
+  );
 }
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,26 +1,41 @@
-import Link from "next/link";
-import * as TabsComponents from "fumadocs-ui/components/tabs";
-import { Step, Steps } from "fumadocs-ui/components/steps";
-import * as AccordionComponents from "fumadocs-ui/components/accordion";
-import * as icons from "lucide-react";
-import * as FilesComponents from "fumadocs-ui/components/files";
-import { Card } from "fumadocs-ui/components/card";
-import { Callout } from "fumadocs-ui/components/callout";
+import { source } from "@/lib/source";
 import { DocsPage, DocsBody, DocsDescription, DocsTitle } from "fumadocs-ui/page";
+import { notFound } from "next/navigation";
+import { getMDXComponents } from "@/mdx-components";
 
-export default function HomePage() {
+export default async function Page(props: { params: Promise<{ slug?: string[] }> }) {
+  const params = await props.params;
+  const page = source.getPage(params.slug);
+  if (!page) notFound();
+
+  const MDXContent = page.data.body;
+  const components = getMDXComponents();
+  console.log(components);
+
   return (
-    <main className="flex flex-1 flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
-      <p className="text-fd-muted-foreground">
-        You can open{" "}
-        <Link
-          href="/docs"
-          className="text-fd-foreground font-semibold underline">
-          /docs
-        </Link>{" "}
-        and see the documentation.
-      </p>
-    </main>
+    <DocsPage
+      toc={page.data.toc}
+      full={page.data.full}>
+      <DocsTitle>{page.data.title}</DocsTitle>
+      <DocsDescription>{page.data.description}</DocsDescription>
+      <DocsBody>
+        <MDXContent components={components} />
+      </DocsBody>
+    </DocsPage>
   );
+}
+
+export async function generateStaticParams() {
+  return source.generateParams();
+}
+
+export async function generateMetadata(props: { params: Promise<{ slug?: string[] }> }) {
+  const params = await props.params;
+  const page = source.getPage(params.slug);
+  if (!page) notFound();
+
+  return {
+    title: page.data.title,
+    description: page.data.description,
+  };
 }
