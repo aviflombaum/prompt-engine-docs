f5c9fc946a70f4a2a72eba0a465720e3dfdef431
Avi Flombaum <4515+aviflombaum@users.noreply.github.com>
2025-07-29 13:11:32 -0400

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,15 +2,10 @@ import "@/app/global.css";
 import { RootProvider } from "fumadocs-ui/provider";
 import type { ReactNode } from "react";
 
-const inter = Inter({
-  subsets: ["latin"],
-});
-
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html
       lang="en"
-      className={inter.className}
       suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
