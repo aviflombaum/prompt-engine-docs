210212a8d4d111f8f05cb40bab98c35537c5f822
Avi Flombaum <4515+aviflombaum@users.noreply.github.com>
2025-07-29 13:00:16 -0400

    fixed build

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -3,11 +3,12 @@ import type { MDXComponents } from "mdx/types";
 import * as TabsComponents from "fumadocs-ui/components/tabs";
 import { Step, Steps } from "fumadocs-ui/components/steps";
 import * as AccordionComponents from "fumadocs-ui/components/accordion";
-import * as icons from "lucide-react";
 import * as FilesComponents from "fumadocs-ui/components/files";
 import { Card } from "fumadocs-ui/components/card";
 import { Callout } from "fumadocs-ui/components/callout";
+import { TypeTable } from "fumadocs-ui/components/type-table";
 import { DocsPage, DocsBody, DocsDescription, DocsTitle } from "fumadocs-ui/page";
+import * as icons from "lucide-react";
 
 // use this function to get MDX components, you will need it for rendering MDX
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
@@ -18,6 +19,7 @@ export function getMDXComponents(components?: MDXComponents): MDXComponents {
     ...FilesComponents,
     ...AccordionComponents,
     DocsPage,
+    TypeTable,
     Callout,
     DocsBody,
     DocsDescription,
