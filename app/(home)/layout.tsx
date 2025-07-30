7e9c9840000bac0cbe3586d283dcd0e9b7dd21fe
Avi Flombaum <4515+aviflombaum@users.noreply.github.com>
2025-07-30 11:41:14 -0400

    more links

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -2,13 +2,49 @@ import { DocsLayout } from "fumadocs-ui/layouts/docs";
 import type { ReactNode } from "react";
 import { baseOptions } from "@/app/layout.config";
 import { source } from "@/lib/source";
+import { HomeLayout } from "fumadocs-ui/layouts/home";
+import Link from "next/link";
+import {
+  NavbarMenu,
+  NavbarMenuContent,
+  NavbarMenuLink,
+  NavbarMenuTrigger,
+} from "fumadocs-ui/layouts/home/navbar";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <DocsLayout
-      tree={source.pageTree}
-      {...baseOptions}>
-      {children}
-    </DocsLayout>
+    <HomeLayout
+      {...baseOptions}
+      links={[
+        {
+          type: "custom",
+          children: (
+            <Link
+              href="https://github.com/aviflombaum/prompt_engine"
+              target="_blank"
+              rel="noopener noreferrer">
+              GitHub
+            </Link>
+          ),
+        },
+        {
+          type: "custom",
+          children: (
+            <Link
+              href="https://prompt-engine-demo.avi.nyc"
+              target="_blank"
+              rel="noopener noreferrer">
+              Demo
+            </Link>
+          ),
+        },
+        // other items
+      ]}>
+      <DocsLayout
+        tree={source.pageTree}
+        {...baseOptions}>
+        {children}
+      </DocsLayout>
+    </HomeLayout>
   );
 }
--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -1,3 +1,5 @@
+import { BookIcon } from 'lucide-react';
+
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
 
 /**
@@ -11,18 +13,10 @@ export const baseOptions: BaseLayoutProps = {
   nav: {
     title: (
       <>
-        <svg
-          width="24"
-          height="24"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-label="Logo"
-        >
-          <circle cx={12} cy={12} r={12} fill="currentColor" />
-        </svg>
-        My App
+        PromptEngine Docs
       </>
     ),
   },
   // see https://fumadocs.dev/docs/ui/navigation/links
-  links: [],
+  links: []
 };
